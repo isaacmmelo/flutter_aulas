6ad0afc48b5c5c7a2b1b646817642e386defdfda
isaacmmelo <isaac.melo@proton.me>
2023-10-01 01:30:44 -0300

    Exclusão Contador App

--- contador_app/README.md
+++
@@ -1,3 +0,0 @@
-# contador_app
-
-A new Flutter project.
--- contador_app/android/app/src/main/kotlin/com/br/ifgoiano/mobile/contador_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.br.ifgoiano.mobile.contador_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
