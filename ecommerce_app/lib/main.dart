718316798d9d19f1a1aa308d5c4fe2c5755073d7
isaacmmelo <isaac.melo@proton.me>
2023-10-02 01:32:09 -0300

    Ecommerce APP: Back4App Config

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -1,11 +1,22 @@
+import 'package:ecommerce_app/cfg/back4app.dart';
 import 'package:ecommerce_app/models/cart.dart';
 import 'package:ecommerce_app/pages/aboutPage.dart';
 import 'package:ecommerce_app/pages/homePage.dart';
 import 'package:ecommerce_app/pages/introPage.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-void main() {
+void main() async {
+  Back4App cfg4app = Back4App();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  final keyApplicationId = cfg4app.keyApplicationId;
+  final keyClientKey = cfg4app.keyClientKey;
+  final keyParseServerUrl = cfg4app.keyParseServerUrl;
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
   runApp(const ECommerceApp());
 }
 
